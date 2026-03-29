ed20a0f9e56163c0cd5263a73da5a58b328cd8a6
Yash Shelke <yashshelke885@google.com>
2026-03-29 23:26:21 +0530

    created backend template

---
+++ server/src/test/java/com/cts/mediconnect/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cts.mediconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
